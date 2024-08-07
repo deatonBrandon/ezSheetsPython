1725c59957f5bdb4dfdf14f27e6fbdb37f3e6cfc
brandon-deaton <dream.cloud936@gmail.com>
2024-08-06 23:01:07 -0700

    while loop confirming random int does not duplicate. Minor edits

--- excel-leads.py
+++ excel-leads.py
@@ -4,8 +4,7 @@ import glob
 import random
 import math
 import numpy as np
-import numpy._core.numeric as _nx
-from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_DOWN, ROUND_HALF_EVEN
+from decimal import Decimal, ROUND_HALF_DOWN
 
 
 # I want to try something different
@@ -17,10 +16,10 @@ folder = glob.glob("./*.csv")
 
 # read csv file from the glob and append it to a list
 def read_data():
+    frame = []
     try:
         for spreadsheet in folder:
             with open(spreadsheet, "r") as infile:
-                frame = []
                 reader = csv.reader(infile)
                 next(reader)
                 for rows in reader:
@@ -35,69 +34,46 @@ def read_data():
 
 
 # Takes a list of lists and iterates through to return a flat list
-def flatten_list(lst):
-    return [x for xs in lst for x in xs]
-
-
-
-    
-# def split_list(ary, indices_or_sections, axis=0):
-#     try:
-#         Ntotal = ary.shape[axis]
-#     except AttributeError:
-#         Ntotal = len(ary)
-#     try:
-#         # handle array case.
-#         Nsections = len(indices_or_sections) + 1
-#         div_points = [0] + list(indices_or_sections) + [Ntotal]
-#     except TypeError:
-#         # indices_or_sections is a scalar, not an array.
-#         Nsections = int(indices_or_sections)
-#         if Nsections <= 0:
-#             raise ValueError('number sections must be larger than 0.') from None
-#         Neach_section, extras = divmod(Ntotal, Nsections)
-#         section_sizes = ([0] +
-#                          extras * [Neach_section+1] +
-#                          (Nsections-extras) * [Neach_section])
-#         div_points = _nx.array(section_sizes, dtype=np.intp).cumsum()
-
-#     sub_arys = []
-#     sary = _nx.swapaxes(ary, axis, 0)
-#     for i in range(Nsections):
-#         st = div_points[i]
-#         end = div_points[i + 1]
-#         sub_arys.append(_nx.swapaxes(sary[st:end], axis, 0))
-
-#     return sub_arys
-
-
-
-
-
+# def flatten_list(lst):
+#     return [x for xs in lst for x in xs]
 
 # split df and chunk leads by number of sales reps
 def split_dataframe(frame):
     try:
         chunks = []   
+        temp_lst = []
 
         chunk_size = int(input("Enter number of sales support reps assigned to this spreadsheet: "))    
-        result = len(frame) / chunk_size
-        result = Decimal(result)
-        result = result.quantize(Decimal("1"), ROUND_HALF_EVEN)
-        result = int(result)
+        result = len(frame) // chunk_size
+        remainder = len(frame) % chunk_size
 
         for bucket in range(0, len(frame), result):
             chunks.append(frame[bucket:bucket + result])
 
-        longest_straw = random.randrange(0, len(chunks) - 1)
-        if (chunks[-1] != 0):
-            test = chunks.pop()
-            popped = flatten_list(test)
-            chunks[longest_straw].append(popped)
+        for i in range(1, remainder + 1):        
+            random_int = random.randint(0, len(chunks) - 1)
+
+            while remainder != 0:
+                if random_int not in temp_lst:
+                    temp_lst.append(random_int) 
+                    remainder -= 1
+                else:  
+                    random_int = random.randint(0, len(chunks) - 1)
+                    continue
+
+            popped = chunks[-1].pop()
+        print(popped)
+        for x in temp_lst:
+            chunks[x].append(popped)
+
+        # print(temp_lst)
 
         # for x in range(len(chunks)):
-        #     print(len(chunks[x]))
+        #     print(len(chunks[x])) 
         #     print(chunks[x])
+
+        # Removes empty array after remainders are removed
+        # chunks.clear()
     except ValueError:
         print("Incorrect type of value. Must be numeric.")
 
